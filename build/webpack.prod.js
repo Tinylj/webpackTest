cc0dca951170060e4900bbd04eec69a1ab604e78
lujin <luj@lhcis.com>
2019-04-08 12:10:41 +0800

    打包css文件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,6 +2,10 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const config = require('../config');
+
 module.exports = merge(common, {
     devtool: 'source-map', // 生产环境最好也加上source-map，如果你不需要可以去掉
     plugins: [
@@ -11,6 +15,14 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
+        // copy custom static assets
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, '../static'),
+                to: config.build.assetsSubDirectory,
+                ignore: ['.*']
+            }
+        ]),
     ],
     output: {
         publicPath: '' // 指定发布路径
